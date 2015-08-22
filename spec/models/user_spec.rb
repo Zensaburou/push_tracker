2085d9b68732d2a4cb4e087246728dbbce02ad35
david <david.jaress@gmail.com>
2015-08-22 14:29:33 -0500

    Uniqueness validation for user name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of :name }
+
+  describe 'uniqueness' do
+    subject { FactoryGirl.build :user }
+    it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+  end
 end
