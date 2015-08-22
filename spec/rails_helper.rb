8617f6e53cac6b9ccd85fd92289a2a64eb2b7eb4
david <david.jaress@gmail.com>
2015-08-22 12:49:00 -0500

    Require shoulda matcher in rails tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
+require 'shoulda/matchers'
 
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
