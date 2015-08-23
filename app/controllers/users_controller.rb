d2867346b9cc91c37ad2204523567e7217b1df1c
david <david.jaress@gmail.com>
2015-08-23 14:11:01 -0500

    Create user service. Post to user#create calls create_unique_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+  def create
+    UserService.new.create_unique_user
+    render nothing: true
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe 'create' do
+    it 'calls create_user' do
+      expect_any_instance_of(UserService).to receive :create_unique_user
+      user_name = Faker::Company.name.tr(' ', '_')
+      post :create, name: user_name
+    end
+  end
 end
