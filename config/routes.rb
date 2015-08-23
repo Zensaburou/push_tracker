57ff963f9880ae853b988a11fb1766880d001f23
david <david.jaress@gmail.com>
2015-08-23 11:31:27 -0500

    Shorten user create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post '/users/create/:name' => 'users#create', as: :user_create
+  post '/users/:name' => 'users#create', as: :user_create
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/config/routes_spec.rb
+++ spec/config/routes_spec.rb
@@ -4,7 +4,7 @@ require 'faker'
 RSpec.describe 'routes', type: :routing do
   it 'creates users' do
     user_name = Faker::Company.name
-    expected_route = '/users/create/' << user_name
+    expected_route = '/users/' << user_name
     expect(post(expected_route)).to route_to(
       controller: 'users',
       action: 'create',
