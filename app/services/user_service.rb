6dddff60dc0748ce46180f2bbb2caa1df446474b
david <david.jaress@gmail.com>
2015-08-23 14:28:48 -0500

    Creates user and returns nil

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -1,4 +1,6 @@
 class UserService
   def create_unique_user(name)
+    User.create!(name: name)
+    nil
   end
 end
--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -1,6 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe UserService do
+  before :example do
+    @service = UserService.new
+  end
+
   describe 'create_unique_user' do
+    before :context do
+      @name = Faker::Company.name.tr(' ', '_')
+    end
+
+    context 'user does not already exist' do
+      it 'creates a new user' do
+        @service.create_unique_user(@name)
+        expected_user = User.where(name: @name)
+        expect(expected_user.count).to eq 1
+      end
+
+      it 'returns nil' do
+        error = @service.create_unique_user(@name)
+        expect(error).to be nil
+      end
+    end
   end
 end
