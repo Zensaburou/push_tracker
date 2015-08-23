ac6fdc6722b7f52598c935e3d68bc2eb48b6c61c
david <david.jaress@gmail.com>
2015-08-23 13:42:38 -0500

    Add versioning to routes

--- spec/config/routes_spec.rb
+++ spec/config/routes_spec.rb
@@ -4,7 +4,7 @@ require 'faker'
 RSpec.describe 'routes', type: :routing do
   it 'creates users' do
     user_name = Faker::Company.name.tr(' ', '_')
-    expected_route = '/' << user_name
+    expected_route = '/v1/' << user_name
     expect(post(expected_route)).to route_to(
       controller: 'users',
       action: 'create',
@@ -15,7 +15,7 @@ RSpec.describe 'routes', type: :routing do
   it 'creates categories' do
     user_name = Faker::Company.name.tr(' ', '_')
     category_name = Faker::Company.bs.tr(' ', '_')
-    expected_route = '/' << user_name << '/' << category_name
+    expected_route = '/v1/' << user_name << '/' << category_name
     expect(post(expected_route)).to route_to(
       controller: 'categories',
       action: 'create',
@@ -27,7 +27,7 @@ RSpec.describe 'routes', type: :routing do
   it 'creates events' do
     user_name = Faker::Company.name.tr(' ', '_')
     category_name = Faker::Company.bs.tr(' ', '_')
-    expected_route = '/' << user_name << '/' << category_name << '/create'
+    expected_route = '/v1/' << user_name << '/' << category_name << '/create'
     expect(post(expected_route)).to route_to(
       controller: 'events',
       action: 'create',
