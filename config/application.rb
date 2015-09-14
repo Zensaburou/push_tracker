bbc32672296ef3be6c6702a3e60b42addb7f697f
david <david.jaress@gmail.com>
2015-09-14 14:56:16 -0500

    Disable assets in application config

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module PushTracker
   class Application < Rails::Application
     config.api_only = true
+
+    config.assets.enabled = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
